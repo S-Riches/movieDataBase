f44e1558b8cc517d5129aea040a89af9fed0d4eb
Sam R <samuelricheswork@gmail.com>
2022-09-01 21:51:44 +0100

    improved the amazon prime search time, much much faster now - also created the sql database and started with the logic on that side

--- scraper Scripts/Main.py
+++ scraper Scripts/Main.py
@@ -1,8 +1,98 @@
 #imports
+#from threading import Thread
 import moviePuller
 from flask import Flask
+from dotenv import load_dotenv
+import os
+import mysql.connector
+from os.path import join, dirname
 
-# this script will function as the api doing the managing for the scraping calls, it will also handle the sql database and will be able to be interfaced via flask.
+# perhaps the script should first scrape the websites, then start the flask app to get that data into the sql table and retrieve whats needed, then we can schedule when to rescrape.
+# this is for the environment variables - meaning i dont have to share the passwords and hostnames for my servers - v good
+dotEnvPath = join(dirname(__file__), '.env')
+# get the env file
+load_dotenv(dotEnvPath) 
+
+# function that sends the data to the sql server.
+def SqlSend(data):
+    # this gets the data from .env and creates a connection to the sql server
+    db = mysql.connector.connect(
+        host = os.environ.get("host"),
+        username = os.environ.get("sqlusername"),
+        password = os.environ.get("password"),
+        database = os.environ.get("database")
+    )
+    # creates a cursor object to command
+    mycursor = db.cursor()
+    # insert command
+    sql = "INSERT INTO movies (site, filmName) VALUES (%s, %s);"
+    # create blank list
+    dataFormat = []
+    #ignores the key values, only puts the values into the sql insert
+    for i in data.values():
+        #add the values to a list
+        dataFormat.append(i)
+    # send the insert request, along with the data that is now formatted to be sent
+    mycursor.execute(sql, dataFormat)
+    # commit the change
+    db.commit()
+    # flavour text
+    print(mycursor, " was inserted.")
+    #close the connection
+    db.close()
+
+# script that gets the films from the scraper script, then puts them into sql db
+def addFilms():
+    #TODO need to check if the film already exists in the db, if so skip it and check the next one - then remove if its not in the current list. this will stop duplication of data
+    # blank list
+    movieJson = []
+    # initial scrapes to populate sql tables
+    # TODO make these scrape all at the same time. - possibly with threading as it will make this faster
+    try:
+        primeList = moviePuller.getPrimeFilms()
+        nowList = moviePuller.getNowTVFilms()
+        all4List = moviePuller.getAll4Films()
+    except Exception:
+        # just print the issue to the console and continue with the other scrapes
+        print(Exception)
+        pass
+        
+    # for each element in the list, turn it into a dictionary
+    for i in primeList:
+        mov = {
+            "site" : "Prime video",
+            "film name" : i
+        }
+        # append it to the list
+        movieJson.append(mov)
+    for i in nowList:
+        mov = {
+            "site" : "Now TV",
+            "film name" : i
+        }
+        movieJson.append(mov)
+    for i in all4List:
+        mov = {
+            "site" : "All4",
+            "film name" : i
+        }
+        movieJson.append(mov)
+
+    # update table if film isnt in there,
+    try:
+        # try to send the film to the sql db
+        for item in movieJson:
+            SqlSend(item)
+    except:
+        print("Film already exists")
+        pass
+
+
+addFilms()
+
+#API SECTION
+
+# this will function as the api doing the managing for the scraping calls, it will also handle the sql database and will be able to be interfaced via flask.
 app = Flask(__name__)
 
 # default path for the api
@@ -10,19 +100,13 @@ app = Flask(__name__)
 def default():
     return "<p> this is default route nerd - try /prime or something</p>"
 
-# these paths will call the films and get them into a list.
-
+# these paths will come back with sql data.
 @app.route("/prime")
-def getPrime():
-    # apparently python doesnt like this - so gonna have to learn scheduling and i SERIOUSLY need to make this async now lmao
-    primeMovies = moviePuller.getPrimeFilms()
-    return "<p>" + primeMovies +"</p>"
-
+def postPrime():
+    return "<p> list = {list} </p>".format(list = primeMovies)
 @app.route("/now")
-def getNow():
-    nowMovies = moviePuller.getNowTVFilms()
-
+def postNow():
+    pass
 @app.route("/all")
-def getAll4():
-    all4Movies = moviePuller.getAll4Films()
-
+def postAll4():
+    pass
--- scraper Scripts/moviePuller.py
+++ scraper Scripts/moviePuller.py
@@ -1,5 +1,7 @@
 # imports
-from requests_html import HTMLSession
+from imp import reload
+from operator import truediv
+from requests_html import HTMLSession, HTMLSession
 import requests
 
 # this function will get the 'free' to watch films from amazon prime, and then put them into a list to then filter through
@@ -11,16 +13,13 @@ def getPrimeFilms():
     url = 'https://www.amazon.co.uk/gp/video/search/ref=atv_sr_filter_p_n_entity_type?phrase=Included%20with%20Prime&ie=UTF8&pageId=default&queryToken=eyJ0eXBlIjoiZmlsdGVyIiwibmF2Ijp0cnVlLCJzZWMiOiJjZW50ZXIiLCJzdHlwZSI6InNlYXJjaCIsInFyeSI6InBfbl93YXlzX3RvX3dhdGNoPTc0NDg2NjIwMzEmbm9kZT0zMjgwNjI2MDMxJnNlYXJjaC1hbGlhcz1pbnN0YW50LXZpZGVvJnBfbl9lbnRpdHlfdHlwZT05NzM5OTUyMDMxIiwidHh0IjoiIiwiZmlsdGVyIjp7fSwib2Zmc2V0IjowLCJucHNpIjowLCJvcmVxIjoiZTJmZTQxMDUtNDBhYS00ZWUzLWFhMmMtZjg0NzU3NGE5NDJkOjE2NjE2MjU5ODQwMDAiLCJzdEtleSI6IntcInNic2luXCI6MCxcImN1cnNpemVcIjowLFwicHJlc2l6ZVwiOjB9Iiwib3JlcWsiOiJtanhETjhaSGRhVGVWWjVrTGVuSHJjWmF1cGpIV2JvZDRxV3BWcHphUWlBPSIsIm9yZXFrdiI6MX0%3D&queryPageType=browse'
     # create a request to pull the html for this page
     r = session.get(url)
-    # now the r object is (due to prime video being a javascript focused page) a lot of code and not a lot of information, so we have to use the render function
     # note we also use these arguements for a few things - the sleep function gives the page some time to load, which stops the page returning nothing but js, along with keep page which allows interaction with browser pages through r.html.page, and scroll down that scrolls down the page
-    # note #2 i realise now it would be much more efficient to just get the content from AJAX instead of scrolling to the bottom but IIABDFI
-    r.html.render(sleep=1, scrolldown=50)
+    r.html.render(sleep=5, script="window.scrollTo(0, document.body.scrollHeight);", reload=True, keep_page=True)
     # find this each of this class in a page and create a list
-    mov = r.html.find('.av-beard-title-link')
-    #TODO perhaps get the year from the scrape and assign it in the dictionary
+    mov = r.html.find('.tst-hover-title')
     # for each item in the list of the movies
     for item in mov:
-        #append movies
+        #append the list
         primeMovies.append(item.text)
     # use this or else headless chromium pages wont close which drains memory
     r.close()
@@ -35,8 +34,7 @@ def getNowTVFilms():
     # create a request to pull the html for this page
     r = session.get(url)
     # now the r object is (due to prime video being a javascript focused page) a lot of code and not a lot of information, so we have to use the render function
-    # TODO this website uses pages that load on scroll to display different films - therefore i need to write code to work out how many films are on one page - then to change page after that many films are scraped
-    # TODO pt 2, tried to get the data from now TV AJAX but keep getting error code 400, bit stumped at the moment but i can still get the data via scraping the page - however at a slower speed.
+    # TODO tried to get the data from now TV AJAX but keep getting error code 400, bit stumped at the moment but i can still get the data via scraping the page - however at a slower speed.
     r.html.render()
     # find this each of this class in a page and create a list
     mov = r.html.find('.ib-card-title')
@@ -87,17 +85,3 @@ def getAll4Films():
     r.close()
     return all4Movies
 
-#debug area
-# TODO make this script async so that we can do multiple scrapes at a time.
-# print("PRIME FILMS -- \n")
-# prime = getPrimeFilms()
-# for i in prime:
-#     print(i)
-# print("NOW TV FILMS -- \n")
-# now = getNowTVFilms()
-# for i in now:
-#     print(i)
-# print("ALL 4 FILMS -- \n")
-# all4 = getAll4Films()
-# for i in all4:
-#     print(i)
