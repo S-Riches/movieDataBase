2f9015a077e47e261cb9eb59d3e3938f371fe8ef
S-Riches <samuelrichesprivate@gmail.com>
2022-09-23 23:53:06 +0100

    wrote more js code to make the film cards more pretty - aswell as made the different sites webpages work

--- frontend/func.js
+++ frontend/func.js
@@ -15,16 +15,18 @@ async function getFilms(site){
 // also create the search function to look for a specific movie and then another for looking for a movie in a list
 async function searchFilm(title){
     // research how to add headers to fetch
-    let response = await fetch(`http://127.0.0.1:5000`)
+    let response = await fetch(`http://127.0.0.1:5000`);
 }
 
 // function to create a card
-function createCard(title, site,boxId){
+function createCard(title, site, boxId){
+    //make the first letter upper case
+    var newTitle = String(title).charAt(0).toUpperCase() + String(title).slice(1);
     // create a card class
-    var card = new Card(title, site);
+    var card = new Card(newTitle, site);
     // create a new element
     const newEl = document.createElement("div");
-    newEl.className += "col card bg-dark text-light";
+    newEl.className += "d-flex justify-content-center text-center col card bg-dark text-light";
     newEl.id += "card";
     // create a title
     const titleContent = document.createElement("h4");
@@ -32,7 +34,6 @@ function createCard(title, site,boxId){
     titleContent.innerText = card.title;
     // styling
     titleContent.className += "card-title";
-    
     const siteContent = document.createElement("p");
     siteContent.innerText = card.site;
     siteContent.className += "card-subtitle"
@@ -72,10 +73,50 @@ function createRow(list){
         }
     }
 }
-// card creation
-getFilms('prime').then(data => {
-    // create a var called list from the returned data
-    var list = data;
-    createRow(list)
-});
+
+// find what page we are on
+var page = window.location.pathname.substring(window.location.pathname.lastIndexOf("/"));
+//console.log(page);
+if(page === "/all.html"){
+    // card creation
+    getFilms('all').then(data => {
+        // create a var called list from the returned data
+        var list = data;
+        createRow(list)
+    });
+}
+if(page === "/now.html"){
+    // card creation
+    getFilms('now').then(data => {
+        // create a var called list from the returned data
+        var list = data;
+        createRow(list)
+    });
+}
+if(page === "/prime.html"){
+    // card creation
+    getFilms('prime').then(data => {
+        // create a var called list from the returned data
+        var list = data;
+        createRow(list)
+    });
+}
+if(page === "/index.html"){
+    // search funtion code goes here
+    /*
+        basic idea for this is to do:
+        - wait for user to press search button
+            - add a listener to check when the button is pressed
+            - take the inner text in the input and run it through data validation
+        - do data validation to ensure that the title is actually a string, isnt sql code, etc
+            - convert to lower case as thats how its stored
+            - research this to find a good method or some other way of sanitizing the input
+        - send the request using the find film function
+            - need to research a way to add headers to the find film function
+            - format the string to allow for the search to work
+            - make sure that there is a good response, if not prompt the user to try again
+        - return the film into the box
+            - just use the function already created
+    */
+}
 
